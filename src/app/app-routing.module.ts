a523cb9880173c16ac3a0808a5a3d2ff50571720
sarthak1009 <sarthakdubey1009@gmail.com>
2022-06-16 22:44:23 +0530

    Admin Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'forgot-password', component: ForgotPasswordComponent},
+  {path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule)},
   {path: '**', component: NotFoundComponent}
 ];
 
